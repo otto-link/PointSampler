c11c631eef4d69686d1662c43fb110d51a6c072c
otto-link <otto-link@users.noreply.github.com>
2025-08-07 12:51:57 +0000

    deploy: 95feadcc4afb353b67f52856caa4549148d27c99

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['example_0',['example',['../namespaceps.html#autotoc_md0',1,'Example'],['../namespaceps.html#autotoc_md1',1,'Example'],['../namespaceps.html#autotoc_md2',1,'Example'],['../namespaceps.html#autotoc_md3',1,'Example'],['../namespaceps.html#autotoc_md4',1,'Example'],['../namespaceps.html#autotoc_md5',1,'Example'],['../namespaceps.html#autotoc_md6',1,'Example'],['../namespaceps.html#autotoc_md7',1,'Example']]]
+  ['example_0',['example',['../namespaceps.html#autotoc_md0',1,'Example'],['../namespaceps.html#autotoc_md1',1,'Example'],['../namespaceps.html#autotoc_md2',1,'Example'],['../namespaceps.html#autotoc_md3',1,'Example'],['../namespaceps.html#autotoc_md4',1,'Example'],['../namespaceps.html#autotoc_md5',1,'Example'],['../namespaceps.html#autotoc_md6',1,'Example'],['../namespaceps.html#autotoc_md7',1,'Example'],['../namespaceps.html#autotoc_md8',1,'Example'],['../namespaceps.html#autotoc_md9',1,'Example'],['../namespaceps.html#autotoc_md10',1,'Example']]]
 ];
