4c5aa025b87df3b6e0af9e2bb1c13949a2d0e748
otto-link <otto-link@users.noreply.github.com>
2025-08-07 09:48:50 +0000

    deploy: 885284d145cc1bc5373513906f5ba5df146978a4

---
+++ html/search/all_f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['your_20project_0',['Add PointSampler to Your Project',['../md_README.html#autotoc_md10',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pointsampler_0',['PointSampler',['../md_README.html',1,'']]]
+];
