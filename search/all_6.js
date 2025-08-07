61f565ab0fa3963993b83d860d61814679d0c3ca
otto-link <otto-link@users.noreply.github.com>
2025-08-07 12:47:08 +0000

    deploy: 15f38a0ed9426f4a14d71dff9ab0d4125a79978b

--- search/all_6.js
+++ search/all_6.js
@@ -3,5 +3,5 @@ var searchData=
   ['importance_5fresampling_0',['importance_resampling',['../namespaceps.html#a932640b18d76a7e336fc760e9cf5511b',1,'ps']]],
   ['importance_5fresampling_2ehpp_1',['importance_resampling.hpp',['../importance__resampling_8hpp.html',1,'']]],
   ['in_5fneighborhood_2',['in_neighborhood',['../namespaceps.html#aedb5908eae325f054e4f3dd6b9b223af',1,'ps']]],
-  ['it_20works_3a_3',['How it works:',['../namespaceps.html#autotoc_md4',1,'']]]
+  ['it_20works_3a_3',['How it works:',['../namespaceps.html#autotoc_md6',1,'']]]
 ];
