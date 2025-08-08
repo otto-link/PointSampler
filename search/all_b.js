ea2a282b5b7c0c7ac19e87c7128f6f16bcb9ef24
otto-link <otto-link@users.noreply.github.com>
2025-08-08 21:39:17 +0000

    deploy: 34a5f7a5c1d7979a2bae219a66ad65e46f930f08

--- search/all_b.js
+++ search/all_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['nanoflann_5fadaptator_2ehpp_0',['nanoflann_adaptator.hpp',['../nanoflann__adaptator_8hpp.html',1,'']]],
-  ['normalized_1',['normalized',['../namespaceps.html#a7de6090e2c8291401816be9bc6cda914',1,'ps']]]
+  ['nearest_5fneighbors_5findices_1',['nearest_neighbors_indices',['../namespaceps.html#ac29744e12116f882d8a620a3fd02d72d',1,'ps']]],
+  ['normalized_2',['normalized',['../namespaceps.html#a7de6090e2c8291401816be9bc6cda914',1,'ps']]]
 ];
--- search/functions_a.js
+++ search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['normalized_0',['normalized',['../namespaceps.html#a7de6090e2c8291401816be9bc6cda914',1,'ps']]]
+  ['nearest_5fneighbors_5findices_0',['nearest_neighbors_indices',['../namespaceps.html#ac29744e12116f882d8a620a3fd02d72d',1,'ps']]],
+  ['normalized_1',['normalized',['../namespaceps.html#a7de6090e2c8291401816be9bc6cda914',1,'ps']]]
 ];
