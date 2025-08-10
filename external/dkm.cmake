c8f2cbb1ff488ac8fef76c1073ac1cdff821a2ef
Otto Link <otto.link.bv@gmail.com>
2025-08-10 20:59:52 +0200

    Add specific target for DKM

--- external/dkm.cmake
+++ external/dkm.cmake
@@ -1,8 +1,8 @@
-project(dkm)
+project(point_sampler_dkm)
 
 set(DKM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dkm/include)
 
 add_library(${PROJECT_NAME} INTERFACE)
-add_library(point_sampler::${PROJECT_NAME} ALIAS ${PROJECT_NAME})
+add_library(point_sampler::dkm ALIAS ${PROJECT_NAME})
 
 target_include_directories(${PROJECT_NAME} INTERFACE ${DKM_DIR})
