ecf1a4ff01f18ece3a69f44485de5e94f881e565
otto-link <otto-link@users.noreply.github.com>
2025-08-18 14:32:08 +0000

    deploy: 62388a45efbd8b83d0daa8d0b5610c4df56d1154

--- search/all_c.js
+++ search/all_c.js
@@ -3,6 +3,5 @@ var searchData=
   ['nanoflann_5fadaptator_2ehpp_0',['nanoflann_adaptator.hpp',['../nanoflann__adaptator_8hpp.html',1,'']]],
   ['nearest_5fneighbors_5findices_1',['nearest_neighbors_indices',['../namespaceps.html#ac29744e12116f882d8a620a3fd02d72d',1,'ps']]],
   ['normalize_5fpoints_2',['normalize_points',['../namespaceps.html#a172358f05108b5cabdac4e43ab309ab2',1,'ps']]],
-  ['normalized_3',['normalized',['../namespaceps.html#a7de6090e2c8291401816be9bc6cda914',1,'ps']]],
-  ['notes_4',['Notes',['../namespaceps.html#autotoc_md11',1,'']]]
+  ['normalized_3',['normalized',['../namespaceps.html#a7de6090e2c8291401816be9bc6cda914',1,'ps']]]
 ];
